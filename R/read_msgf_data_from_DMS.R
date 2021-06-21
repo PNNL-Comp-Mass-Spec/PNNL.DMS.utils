af10ab73f768bacca85776defcf4dbeca0567aa2
Vlad Petyuk <petyuk@gmail.com>
2021-06-21 14:59:33 -0700

    reader of MSGF jobs now accepts param_file as an optional argument

--- R/read_msgf_data_from_DMS.R
+++ R/read_msgf_data_from_DMS.R
@@ -1,6 +1,9 @@
 #' Reading MSGF output from PNNL's DMS
 #'
 #' @param DataPkgNumber (Numeric or Character vector) containing Data Package ID(s) located in DMS
+#' @param param_file (character) MS-GF+ parameter file. 
+#'                   If the data package jobs refer to only one parameter 
+#'                   file, then NULL (default) can be accepted as the argument.
 #' @return (MSnID) MSnID object
 #' @importFrom PlexedPiper convert_msgf_output_to_msnid
 #' 
@@ -10,7 +13,7 @@
 #' head(MSnID::psms(msnid))
 
 #' @export
-read_msgf_data_from_DMS <- function(DataPkgNumber) {
+read_msgf_data_from_DMS__old <- function(DataPkgNumber) {
   # Fetch job records for data package(s)
   if (length(DataPkgNumber) > 1) {
     job_rec_ls <- lapply(DataPkgNumber, get_job_records_by_dataset_package)
@@ -28,4 +31,42 @@ read_msgf_data_from_DMS <- function(DataPkgNumber) {
   msnid <- convert_msgf_output_to_msnid(results)
   return(msnid)
 }
+
+
+
+read_msgf_data_from_DMS <- function(DataPkgNumber, param_file = NULL) {
+  # Fetch job records for data package(s)
+  if (length(DataPkgNumber) > 1) {
+    job_rec_ls <- lapply(DataPkgNumber, get_job_records_by_dataset_package)
+    jobRecords <- Reduce(rbind, job_rec_ls)
+  } else {
+    jobRecords <- get_job_records_by_dataset_package(DataPkgNumber)
+  }
+  
+  jobRecords <- jobRecords[grepl("MSGFPlus", jobRecords$Tool),]
+  
+  # THIS PARAMETER ARGUMENT CHECK CAN BE MORE ELEGANT
+  # Check parameter file. Is there any redundancy?
+  param_files <- unique(jobRecords$Parameter_File)
+  if(length(param_files) > 1 & is.null(param_file))
+    stop("There are multiple parameter files and exact one to use isn't specified!")
+  # it proceeds to this point if `param_file` isn't NULL
+  if(length(param_files) > 1 & !(param_file %in% param_files))
+    stop("There are multiple parameter, but the specified one isn't one of them!")
+  if(!is.null(param_file) & param_file != param_files)
+    stop("There specified parameter file ins't the one that is in the data package!")
+  # at this point everything should be fine
+  if(length(param_files) == 1)
+    param_file <- unique(jobRecords$Parameter_File)
+  
+  jobRecords <- jobRecords[jobRecords$Parameter_File == param_file,]
+  
+  results <- get_results_for_multiple_jobs.dt(jobRecords) 
+  tool <- unique(jobRecords$Tool)
+  pattern <- tool2suffix[[tool]]
+  results <- results[[pattern]]
+  msnid <- convert_msgf_output_to_msnid(results)
+  return(msnid)
+}
+
 
