88784ed7c57011e53945a76ab3f1817aabfe0b39
Michael Nestor <47347033+mdnestor@users.noreply.github.com>
2021-06-23 17:15:35 -0700

    removed MSnID dependency

--- R/read_msgf_data_from_DMS.R
+++ R/read_msgf_data_from_DMS.R
@@ -1,22 +1,20 @@
 #' Reading MSGF output from PNNL's DMS
 #'
-#' @param DataPkgNumber (Numeric or Character vector) containing Data Package ID(s) located in DMS
-#' @return (MSnID) MSnID object
-#' @importFrom PlexedPiper convert_msgf_output_to_msnid
+#' @param data_package_num (Numeric or Character vector) containing Data Package ID(s) located in DMS
+#' @return (data.frame) concatenated job results
 #' 
 #' @examples
-#' msnid <- read_msgf_data_from_DMS(3442)
-#' print(msnid)
-#' head(MSnID::psms(msnid))
+#' results <- read_msgf_data_from_DMS(3442)
+#' head(results)
 
 #' @export
-read_msgf_data_from_DMS <- function(DataPkgNumber) {
+read_msgf_data_from_DMS <- function(data_package_num) {
   # Fetch job records for data package(s)
-  if (length(DataPkgNumber) > 1) {
-    job_rec_ls <- lapply(DataPkgNumber, get_job_records_by_dataset_package)
+  if (length(data_package_num) > 1) {
+    job_rec_ls <- lapply(data_package_num, get_job_records_by_dataset_package)
     jobRecords <- Reduce(rbind, job_rec_ls)
   } else {
-    jobRecords <- get_job_records_by_dataset_package(DataPkgNumber)
+    jobRecords <- get_job_records_by_dataset_package(data_package_num)
   }
 
   jobRecords <- jobRecords[grepl("MSGFPlus", jobRecords$Tool),]
@@ -25,7 +23,6 @@ read_msgf_data_from_DMS <- function(DataPkgNumber) {
   tool <- unique(jobRecords$Tool)
   pattern <- tool2suffix[[tool]]
   results <- results[[pattern]]
-  msnid <- convert_msgf_output_to_msnid(results)
-  return(msnid)
+  return(results)
 }
 
