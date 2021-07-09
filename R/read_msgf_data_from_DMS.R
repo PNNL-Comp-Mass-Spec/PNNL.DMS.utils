858d7d76e93e5056c5b869ca6aeec3269b3cc943
Tyler Sagendorf <57922480+TylerSagendorf@users.noreply.github.com>
2021-07-09 14:00:51 -0700

    Update read_msgf_data_from_DMS
    The parameter data_package_num was not used, and the param DataPkgNumber did not exist in the function definition; changed the latter to the former. Modified param_file input checks so that the user does not have to provide a parameter file if there is only one. Use the PlexedPiper test data package in the example because it is smaller.

--- R/read_msgf_data_from_DMS.R
+++ R/read_msgf_data_from_DMS.R
@@ -11,17 +11,17 @@
 #' @importFrom MSnID convert_msgf_output_to_msnid
 #' 
 #' @examples
-#' msnid <- read_msgf_data_from_DMS(3442)
+#' msnid <- read_msgf_data_from_DMS(3606)
 #' show(msnid)
 
 #' @export
 read_msgf_data_from_DMS <- function(data_package_num, param_file = NULL) {
   # Fetch job records for data package(s)
-  if (length(DataPkgNumber) > 1) {
-    job_rec_ls <- lapply(DataPkgNumber, get_job_records_by_dataset_package)
+  if (length(data_package_num) > 1) {
+    job_rec_ls <- lapply(data_package_num, get_job_records_by_dataset_package)
     jobRecords <- Reduce(rbind, job_rec_ls)
   } else {
-    jobRecords <- get_job_records_by_dataset_package(DataPkgNumber)
+    jobRecords <- get_job_records_by_dataset_package(data_package_num)
   }
   
   jobRecords <- jobRecords[grepl("MSGFPlus", jobRecords$Tool),]
@@ -29,18 +29,27 @@ read_msgf_data_from_DMS <- function(data_package_num, param_file = NULL) {
   # THIS PARAMETER ARGUMENT CHECK CAN BE MORE ELEGANT
   # Check parameter file. Is there any redundancy?
   param_files <- unique(jobRecords$Parameter_File)
-  if(length(param_files) > 1 & is.null(param_file))
-    stop("There are multiple parameter files and exact one to use isn't specified!")
-  # it proceeds to this point if `param_file` isn't NULL
-  if(length(param_files) > 1 & !(param_file %in% param_files))
-    stop("There are multiple parameter, but the specified one isn't one of them!")
-  if(!is.null(param_file) & param_file != param_files)
-    stop("There specified parameter file ins't the one that is in the data package!")
-  # at this point everything should be fine
-  if(length(param_files) == 1)
-    param_file <- unique(jobRecords$Parameter_File)
   
-  jobRecords <- jobRecords[jobRecords$Parameter_File == param_file,]
+  # Check if param_file is NULL
+  if (is.null(param_file)) {
+    # If there is a single parameter file, assign it to param_file
+    if (length(param_files) == 1) {
+      param_file <- param_files
+    } else {
+      # If there are multiple parameter files, and param_file is NULL,
+      # halt execution and print an error message.
+      stop(paste("There are multiple parameter files.", 
+                 "Please specify which parameter file to use with param_file.")
+      )
+    }
+  } else {
+    # If param_file is not NULL, check validity.
+    if (!(param_file %in% param_files)) {
+      stop(paste0("\"", param_file, "\"", " is not a valid parameter file."))
+    }
+  }
+  
+  jobRecords <- jobRecords[jobRecords$Parameter_File == param_file, ]
   
   results <- get_results_for_multiple_jobs.dt(jobRecords) 
   tool <- unique(jobRecords$Tool)
