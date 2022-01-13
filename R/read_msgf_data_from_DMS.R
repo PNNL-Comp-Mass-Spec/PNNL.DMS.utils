9fd7bde1684b4d3456d74ce3a9c31c9cbf4c3642
Tyler Sagendorf <57922480+TylerSagendorf@users.noreply.github.com>
2022-01-13 12:24:19 -0800

    Add use_mzIdentML argument to read_msgf_data_from_DMS
    Also cleaned up the DESCRIPTION file to remove unused fields.

--- R/read_msgf_data_from_DMS.R
+++ R/read_msgf_data_from_DMS.R
@@ -1,25 +1,40 @@
 #' Reading MS-GF+ output from PNNL's DMS
 #'
-#' @param data_package_num (Numeric or Character vector) containing Data 
-#'        Package ID(s) located in DMS.
-#' @param param_file (character) MS-GF+ parameter file. No need to specify this 
-#'        if there is only one parameter file associated with the jobs.
-#' @param organism_db (character) FASTA file. This is the same as the 
-#'        \code{Organism DB} column. No need to specify this if there is only 
-#'        one FASTA file associated with the jobs.
-#' 
+#' @param data_package_num numeric or character; the Data Package ID in the DMS.
+#' @param param_file character; MS-GF+ parameter file. No need to specify this
+#'   if there is only one parameter file associated with the jobs.
+#' @param organism_db character; FASTA file name. This is the same as the
+#'   \code{Organism DB} column. No need to specify this if there is only one
+#'   FASTA file associated with the jobs.
+#' @param use_mzIdentML logical; whether to read mzIdentML files into `psms`
+#'   `data.table` slot of the `MSnID`.
+#'
+#' @details If `use_mzIdentML = TRUE`, a progress bar will not be displayed
+#'   while the results are being fetched. Also, the table in the `psms` slot
+#'   will not be the same as when `mzIdentML = FALSE`. As a result, it may
+#'   require additional modification by the user to prepare it for downstream
+#'   processing steps.
+#'
 #' @return (MSnID) MSnID object
-#' 
-#' @importFrom MSnID convert_msgf_output_to_msnid
-#' 
+#'
+#' @md
+#'
+#' @importFrom MSnID convert_msgf_output_to_msnid read_mzIDs MSnID
+#' @importFrom data.table data.table
+#'
 #' @examples
 #' msnid <- read_msgf_data_from_DMS(3606)
+#' show(msnid)
+#'
+#' #
+#' msnid <- read_msgf_data_from_DMS(3606, use_mzIdentML = TRUE)
 #' show(msnid)
 
 #' @export
 read_msgf_data_from_DMS <- function(data_package_num, 
                                     param_file = NULL,
-                                    organism_db = NULL) {
+                                    organism_db = NULL,
+                                    use_mzIdentML = FALSE) {
   # Fetch job records for data package(s)
   if (length(data_package_num) > 1) {
     job_rec_ls <- lapply(data_package_num, get_job_records_by_dataset_package)
@@ -71,7 +86,7 @@ read_msgf_data_from_DMS <- function(data_package_num,
   } else {
     # If organism_db is not NULL, check validity.
     if (!(organism_db %in% organism_dbs)) {
-      stop(paste0("\"", organism_db, "\"", " is not a valid FASTA file."))
+      stop(sprintf("'%s' is not a valid FASTA file.", organism_db))
     }
   }
   
@@ -79,12 +94,28 @@ read_msgf_data_from_DMS <- function(data_package_num,
   jobRecords <- jobRecords[jobRecords$Parameter_File == param_file &
                              jobRecords$`Organism DB` == organism_db, ]
   
-  results <- get_results_for_multiple_jobs.dt(jobRecords) 
-  tool <- unique(jobRecords$Tool)
-  pattern <- tool2suffix[[tool]]
-  results <- results[[pattern]]
-  msnid <- convert_msgf_output_to_msnid(results)
+  if (use_mzIdentML) {
+    mzid_files <- list.files(jobRecords$Folder, pattern = ".mzid.gz", 
+                             full.names = TRUE)
+    
+    # Read mzIdentML files into psms slot
+    # Note: adding a progress bar with llply drastically  
+    # increases memory usage and time
+    suppressMessages(
+      msnid <- MSnID::read_mzIDs(MSnID(), mzid_files, backend = "mzR")
+    )
+  } else {
+    results <- get_results_for_multiple_jobs.dt(jobRecords) 
+    tool <- unique(jobRecords$Tool)
+    pattern <- tool2suffix[[tool]]
+    results <- results[[pattern]]
+    msnid <- convert_msgf_output_to_msnid(results)
+  }
   return(msnid)
 }
+
+
+
+
 
 
