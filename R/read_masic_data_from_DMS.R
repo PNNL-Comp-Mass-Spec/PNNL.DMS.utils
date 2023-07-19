04b65b89dd304daa677d41d9eb7f685f572f1b4c
skelly001 <97435404+skelly001@users.noreply.github.com>
2023-07-18 22:16:37 -0700

    update namespace and example

--- R/read_masic_data_from_DMS.R
+++ R/read_masic_data_from_DMS.R
@@ -2,11 +2,15 @@
 #'
 #' @param data_package_num (numeric or character vector) containing Data Package
 #'   ID(s) located in DMS.
+#'   
 #' @param interference_score (logical) read interference score. Default is
 #'   \code{FALSE}.
-#' @param id_quant_table If the MS/MS identification scans and reporter ion
-#'   intensity scans are different for a particular dataset, set this to
-#'   \code{TRUE}. Default is \code{FALSE}.
+#'   
+#' @param idscanpattern Ion fragmentation method used to produce the scan used for 
+#'    peptide identification
+#'    
+#' @param quantScanPattern Ion fragmentation method used to produce the scan used for 
+#'    peptide quantification
 #'
 #' @return (data.frame) with reporter ion intensities and other metrics
 #'
@@ -14,54 +18,62 @@
 #' @importFrom data.table rbindlist
 #'
 #' @export read_masic_data_from_DMS
+#' 
+#' @examples
+#' if (is_PNNL_DMS_connection_successful()) {
+#' test <- read_masic_data_from_DMS(4970)
+#' }
 
 
 read_masic_data_from_DMS <- function(data_package_num, 
                                      interference_score=FALSE, 
-                                     id_quant_table = FALSE)
+                                     idScanPattern = "ms2", 
+                                     quantScanPattern = "ms2")
 {
-  # Prevent "no visible binding for global variable" note
-  Dataset <- Scan <- QuantScan <- IDScan <- 
-    FragScanNumber <- ScanNumber <- NULL
-  
-  on.exit(gc(), add = TRUE)
-  
-  # Fetch job records for data package(s)
-  if(length(data_package_num) > 1){
-    jobRecords <- lapply(data_package_num, get_job_records_by_dataset_package)
-    jobRecords <- rbindlist(jobRecords)
-  } else {
-    jobRecords <- get_job_records_by_dataset_package(data_package_num)
-  }
-  
-  jobRecords <- jobRecords[grepl("MASIC", jobRecords$Tool),]
-  
-  # select relevant columns from df, drop redundant dataset column (select(-2))
-  masicList <- get_results_for_multiple_jobs.dt(jobRecords)
-  masicData <- masicList[["_ReporterIons.txt"]] %>% 
-    select(Dataset, 
-           ScanNumber, 
-           starts_with("Ion"), 
-           -contains("Resolution"))
-  
-  if (id_quant_table) {
-    quantIDTable <- make_id_to_quant_scan_link_table(masicList[["_ScanStatsEx.txt"]]) %>%
-      dplyr::rename(any_of(c("ScanNumber" = "QuantScan")))
-    masicData <- inner_join(quantIDTable, masicData, 
-                            by = c("Dataset", "ScanNumber")) %>%    
-      select(-ScanNumber) %>%    
-      dplyr::rename(ScanNumber = IDScan)
-  }
-  
-  if (interference_score) {
-    masicStats <- masicList[["_SICstats.txt"]] %>% 
-      # select(-2) %>%
-      dplyr::rename(any_of(c("ScanNumber" = "FragScanNumber"))) %>% 
-      select(Dataset, ScanNumber, contains("InterferenceScore"))
-    masicData <- inner_join(masicData, masicStats, 
-                            by = c("Dataset", "ScanNumber"))
-  }
-  return(masicData)
+   # Prevent "no visible binding for global variable" note
+   Dataset <- Scan <- QuantScan <- IDScan <- 
+      FragScanNumber <- ScanNumber <- NULL
+   
+   on.exit(gc(), add = TRUE)
+   
+   # Fetch job records for data package(s)
+   if(length(data_package_num) > 1){
+      jobRecords <- lapply(data_package_num, get_job_records_by_dataset_package)
+      jobRecords <- rbindlist(jobRecords)
+   } else {
+      jobRecords <- get_job_records_by_dataset_package(data_package_num)
+   }
+   
+   jobRecords <- jobRecords[grepl("MASIC", jobRecords$Tool),]
+   
+   # select relevant columns from df, drop redundant dataset column (select(-2))
+   masicList <- get_results_for_multiple_jobs.dt(jobRecords)
+   masicData <- masicList[["_ReporterIons.txt"]] %>% 
+      select(Dataset, 
+             ScanNumber, 
+             starts_with("Ion"), 
+             -contains("Resolution"))
+   
+   if (idScanPattern != quantScanPattern) {
+      quantIDTable <- make_id_to_quant_scan_link_table(masicList[["_ScanStatsEx.txt"]],
+                                                       idScanPattern = idScanPattern,
+                                                       quantScanPattern = quantScanPattern) %>%
+         dplyr::rename(any_of(c("ScanNumber" = "QuantScan")))
+      masicData <- inner_join(quantIDTable, masicData, 
+                              by = c("Dataset", "ScanNumber")) %>%    
+         select(-ScanNumber) %>%    
+         dplyr::rename(ScanNumber = IDScan)
+   }
+   
+   if (interference_score) {
+      masicStats <- masicList[["_SICstats.txt"]] %>% 
+         # select(-2) %>%
+         dplyr::rename(any_of(c("ScanNumber" = "FragScanNumber"))) %>% 
+         select(Dataset, ScanNumber, contains("InterferenceScore"))
+      masicData <- inner_join(masicData, masicStats, 
+                              by = c("Dataset", "ScanNumber"))
+   }
+   return(masicData)
 }
 
 
